a391c56e8b752d1f38ba465cb73e9647352760b7
Matheu Plouffe <matheuplouffe@gmail.com>
2020-05-30 20:06:52 -0500

    Moving platforms. WIP.

--- test-pattern-generator/tstptrn_generator.js
+++ test-pattern-generator/tstptrn_generator.js
@@ -1,4 +1,5 @@
 const generatePalette = require('tstptrn_palette_generator');
+const generateTestBars = require('tstprtn_image_compositor');
 
 const generateTestPattern = () => {
     let palette = generatePalette(6);
